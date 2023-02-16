3bd31f15593005e31b454871ae22a3c80100c25b
smartyJOYZ <joysmarty2022@gmail.com>
2023-02-16 13:58:22 -0800

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints the size of various types
+ * main - A program that prints the size of various types on the computer
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -16,7 +16,7 @@ printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
-	return (0);
+return (0);
 }
 
 
