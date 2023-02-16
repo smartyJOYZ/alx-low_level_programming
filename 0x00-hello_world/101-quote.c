92143f1a7d0b6e4b65c61c5ab2402a86e34f55f0
smartyJOYZ <joysmarty2022@gmail.com>
2023-02-16 15:32:21 -0800

    101-quote.c;

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A C program that prints exactly to the standard error
- * Return: Always 1 (Success)
+ * Return:  Always 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19 ", stdout);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
