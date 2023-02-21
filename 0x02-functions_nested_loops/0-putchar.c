9a840f99054987daba07217a54e301aed1322a5d
smartyJOYZ <joysmarty2022@gmail.com>
2023-02-21 10:10:49 -0800

    ./0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A program that prints _putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
