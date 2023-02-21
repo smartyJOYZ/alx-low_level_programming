2d3acb7da691600e330aaab5059e4b5b4d55d54d
smartyJOYZ <joysmarty2022@gmail.com>
2023-02-21 06:27:20 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A program that determines if a number is +ve,0 or -ve
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0 ; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
