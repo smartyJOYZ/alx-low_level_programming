a2c7f58470fa98a3abbb705653ad87f060d3a3ec
smartyJOYZ <joysmarty2022@gmail.com>
2023-02-22 06:57:08 -0800

    ./0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Description: A program that prints _putchar
- * Return: Always 0 (Success)
+ * main - print _putchar
+ * description: the character to print
+ * Return: Always 0 Success
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
