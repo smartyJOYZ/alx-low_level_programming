b6d6ccec65e817807a0d8b8357f596e0f33fe605
smartyJOYZ <joysmarty2022@gmail.com>
2023-02-22 07:29:03 -0800

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prnts alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char alpha;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+	}
+}
