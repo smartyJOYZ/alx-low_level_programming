b23c557324a88e49292e27abea500ce4421eb8c3
smartyJOYZ <joysmarty2022@gmail.com>
2023-02-22 23:01:37 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - a function that prints the last digit of a number
+ * @a : is the char to be checked
+ * @b: a number
+ * Return: always 0
+ */
+int add(int a, int b)
+{
+	int add;
+
+	add = a + b;
+	return (add);
+}
