805bf5f9ffef6f22b83d6bf1b76118222588eeca
smartyJOYZ <joysmarty2022@gmail.com>
2023-02-22 22:51:30 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - a function that prints the last digit of a number
+ * description : is the char to be checked
+ * Return: 0 success
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+		k = i * j;
+		if (j == 0)
+		{
+			_putchar(k + '0');
+		}
+		if (k < 10 && j != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(k + '0');
+		}
+		else if (k >= 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+		}
+	}
+		_putchar('\n');
+	}
+}
