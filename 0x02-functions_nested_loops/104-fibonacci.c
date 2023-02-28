27175279bf15772dfb66933ba8d4d984c0d49d6a
smartyJOYZ <joysmarty2022@gmail.com>
2023-02-28 08:15:51 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,15 +1,14 @@
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
  * main - a function that prints the last digit of a number
- * description : is the char to be checked
+ * purpose - no hardcore
  * Return: 0 success
  */
 int main(void)
-{
-	unsigned long a = 1, b = 2, c;
-	unsigned long a1, a2, b1, b2, l = 1000000000;
+{  
+	unsigned long int a = 1, b = 2, c;
+	unsigned long int a1, a2, b1, b2, l = 1000000000;
 	int i, count = 0;
 
 	printf("%lu", a);
@@ -19,14 +18,14 @@ int main(void)
 		a += b;
 		b = a - b;
 	}
-	b1 = (b / 1);
-	b2 = (b % 1);
-	a1 = (a / 1);
-	a2 = (a % 1);
+	b1 = (b / l);
+	b2 = (b % l);
+	a1 = (a / l);
+	a2 = (a % l);
 	for (i = 92; i < 99; i++)
 	{
 		printf(", %lu", a1 + (a2 / l));
-		printf("%lu", a2 % 1);
+		printf("%lu", a2 % l);
 		a1 = a1 + b1;
 		b1 = a1 - b1;
 		a2 = a2 + b2;
