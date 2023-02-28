ae8d0b2f3386edd85a6aa9536182bbfed82d2f6a
smartyJOYZ <joysmarty2022@gmail.com>
2023-02-28 08:53:03 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,30 +6,40 @@
  * Return: 0 success
  */
 int main(void)
-{  
-	unsigned long int a = 1, b = 2, c;
-	unsigned long int a1, a2, b1, b2, l = 1000000000;
-	int i, count = 0;
+{
+	unsigned long a = 0, b = 1, c;
+	unsigned long a1, a2, b1, b2, d1, d2, l = 10000000000;
+	int i;
 
-	printf("%lu", a);
-	for (i = 1; i < 91; i++)
+	for (i = 0; i < 92; i++)
 	{
-		printf(" ,%lu", b);
-		a += b;
-		b = a - b;
+		c = a + b;
+		printf("%lu", c);
+		a = b;
+		b = c;
 	}
 	b1 = (b / l);
 	b2 = (b % l);
 	a1 = (a / l);
 	a2 = (a % l);
-	for (i = 92; i < 99; i++)
+	for (i = 93; i < 99; i++)
 	{
-		printf(", %lu", a1 + (a2 / l));
-		printf("%lu", a2 % l);
-		a1 = a1 + b1;
-		b1 = a1 - b1;
-		a2 = a2 + b2;
-		b2 = a2 - b2;
+		d1 = a1 + b1;
+		d2 = a2 + b2;
+		if (d2 >= l)
+		{
+			d1 += 1;
+			d2 %= l;
+		}
+		printf("%lu%lu", d1, d2);
+		if (i != 98)
+		{
+			printf(",");
+		}
+		a1 = b1;
+		a2 = b2;
+		b1 = d1;
+		b2 = d2;
 	}
 	printf("\n");
 	return (0);
