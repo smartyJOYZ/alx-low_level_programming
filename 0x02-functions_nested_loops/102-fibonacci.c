0a26f17291870a3316b9651d0d1dc0c7376b41d3
smartyJOYZ <joysmarty2022@gmail.com>
2023-02-28 02:26:42 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a function that prints the last digit of a number
+ * description : is the char to be checked
+ * Return: 0 success
+ */
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int c;
+	int count = 0;
+
+	while (count < 50)
+	{
+		c = a + b;
+		printf("%d + %d = %d\n", a, b, c);
+	}
+	count++;
+	a = b;
+	b = c;
+	return (0);
+}
