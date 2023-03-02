640c2058ef7e1cd693beb0913c0ea869f3f8ebe1
smartyJOYZ <joysmarty2022@gmail.com>
2023-03-02 00:21:45 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: a value
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
